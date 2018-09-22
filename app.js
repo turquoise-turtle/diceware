6b5cb7644c6ccddaa0d8bf6eddda7bf10656a724
turquoise-turtle <a3j2p1@gmail.com>
2018-09-22 17:41:14 +1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,126 @@
+if ('serviceWorker' in navigator) {
+	window.addEventListener('load', function() {
+		navigator.serviceWorker.register('/diceware/service-worker.js')
+		.then(function (reg){
+			console.log('sw registered:', reg);
+		}, /*catch*/ function(error) {
+			console.log('Service worker registration failed:', error);
+		});
+	});
+}
+
+var app = new Vue({
+	el: '#app',
+	data: {
+		dicewarelist: {
+			starwars: {
+				minmax: [1,20],
+				number: 3,
+				list: window.starwars,
+				join: '-'
+			},
+			largewordlist: {
+				minmax: [1,6],
+				number: 5,
+				list: window.eff_large_wordlist,
+				join: ''
+			},
+			shortwordlist1: {
+				minmax: [1,6],
+				number: 4,
+				list: window.eff_short_wordlist_1,
+				join: ''
+			},
+			shortwordlist2: {
+				minmax: [1,6],
+				number: 4,
+				list: window.eff_short_wordlist_2,
+				join: ''
+			},
+			originaldiceware: {
+				minmax: [1,6],
+				number: 5,
+				list: window.diceware_wordlist,
+				join: ''
+			}
+		},
+		phraselength: 5,
+		selected: 'starwars',
+		options: {
+			1: {
+				name: 'Star Wars',
+				shortcode: 'starwars'
+			},
+			2: {
+				name: 'EFF Large Wordlist',
+				shortcode: 'largewordlist'
+			},
+			3: {
+				name: 'EFF Short Wordlist #1',
+				shortcode: 'shortwordlist1'
+			},
+			4: {
+				name: 'EFF Short Wordlist #2',
+				shortcode: 'shortwordlist2'
+			},
+			5: {
+				name: 'Original Diceware Wordlist',
+				shortcode: 'originaldiceware'
+			}
+		},
+		curcode: [],
+		manual: ''
+	},
+	methods: {
+		cryptorandom: function(min, max) {
+			/* https://github.com/password-diet/password.diet/blob/master/app/scripts/components/home.js */
+			var byteArray = new Uint8Array(1);
+			window.crypto.getRandomValues(byteArray);
+			var range = max - min + 1;
+			var max_range = 256;
+			if (byteArray[0] >= Math.floor(max_range / range) * range) {
+				return this.cryptorandom(min, max);
+			}
+			return min + (byteArray[0] % range);
+		},
+		newrandom: function() {
+			this.curcode = [];
+			for (var j = 0; j<this.phraselength; j++) {
+				var dwlist = this.dicewarelist[this.selected];
+				var nums = dwlist.minmax;
+				var num = dwlist.number;
+				var newcode = [];
+				for (var i = 0; i<num; i++) {
+					newcode.push(this.cryptorandom(nums[0], nums[1]));
+				}
+				this.curcode.push(newcode);
+			}
+		}
+	},
+	watch: {
+		selected: function (newv, oldv) {
+			this.newrandom();
+		}
+	},
+	computed: {
+		phrase: function() {
+			var sentence = '';
+			for (var code of this.curcode) {
+				code = code.join(this.dicewarelist[this.selected].join);
+				sentence = sentence + this.dicewarelist[this.selected].list[code] + ' ';
+			}
+			sentence = sentence.substring(0, sentence.length-1);
+			return sentence;
+			//return this.dicewarelist[this.selected].list[this.curcode]
+		},
+		evaluated: function() {
+			var code = this.manual.split(',').join(this.dicewarelist[this.selected].join);
+			var word = this.dicewarelist[this.selected].list[code]
+			return word;
+		}
+	},
+	created: function () {
+		this.newrandom();
+	}
+	//dicewarelist[selected].list[curcode]
+});
---
+++ service-worker.js
@@ -0,0 +1,71 @@
+var CACHE = 'cache-and-update-diceware';
+
+// On install, cache some resources.
+self.addEventListener('install', function(evt) {
+  console.log('The service worker is being installed.');
+
+  // Ask the service worker to keep installing until the returning promise
+  // resolves.
+  evt.waitUntil(precache());
+});
+
+// On fetch, use cache but update the entry with the latest contents
+// from the server.
+self.addEventListener('fetch', function(evt) {
+  console.log('The service worker is serving the asset.');
+  // You can use `respondWith()` to answer immediately, without waiting for the
+  // network response to reach the service worker...
+  evt.respondWith(fromCache(evt.request));
+  // ...and `waitUntil()` to prevent the worker from being killed until the
+  // cache is updated.
+  evt.waitUntil(update(evt.request));
+});
+
+// Open a cache and use `addAll()` with an array of assets to add all of them
+// to the cache. Return a promise resolving when all the assets are added.
+function precache() {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.addAll([
+      '/diceware/index.html',
+      '/diceware/app.js',
+      '/diceware/app.css',
+      '/diceware/vue.js',
+      '/diceware/starwars.js',
+      '/diceware/eff_large_wordlist.js',
+      '/diceware/eff_short_wordlist_1.js',
+      '/diceware/eff_short_wordlist_2_0.js',
+      '/diceware/diceware.wordlist.js',
+      '/diceware/favicon-16x16.png',
+      '/diceware/favicon-32x32.png',
+      '/diceware/favicon.ico'
+    ]);
+  });
+}
+
+// Open the cache where the assets were stored and search for the requested
+// resource. Notice that in case of no matching, the promise still resolves
+// but it does with `undefined` as value.
+function fromCache(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      console.log('fromcache request', request);
+      return matching || Promise.reject('no-match');
+    });
+  });
+}
+
+// Update consists in opening the cache, performing a network request and
+// storing the new response data.
+function update(request) {
+  return caches.open(CACHE).then(function (cache) {
+    return fetch(request).then(function (response) {
+      return cache.put(request, response);
+    }).catch(function(error){
+    	console.log(error);
+    	//do nothing
+    	return new Promise(function(resolve,reject){
+    		resolve();
+    	});
+    });
+  });
+}
